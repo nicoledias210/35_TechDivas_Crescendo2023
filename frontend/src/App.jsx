94216011c3915168a1ec73f202aa4684e5684bb6
nicole <nicole.dias210@gmail.com>
2023-03-12 01:50:30 +0530

    pages added

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -7,8 +7,12 @@ import Analytics from './pages/Analytics'
 import {Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import About from './pages/About';
-
-  
+import Metal from './components/Metal';
+import Plastic from './components/Plastic';
+import Paper from './components/Paper';
+import Cardboard from './components/Cardboard';
+import Glass from './components/Glass';
+import Trash from './components/Trash';
 function App() {
   return (
     <>
@@ -17,6 +21,12 @@ function App() {
      <Route path = "/Report" element = {<Report/>} />
      <Route path = "/Analytics" element = {<Analytics/>} />
      <Route path = "/Auth" element = {<Auth/>} />
+     <Route path = "/Waste/Metal" element = {<Metal />} />
+     <Route path = "/Waste/Plastic" element = {<Plastic />} />
+     <Route path = "/Waste/Paper" element = {<Paper />} />
+     <Route path = "/Waste/Cardboard" element = {<Cardboard />} />
+     <Route path = "/Waste/Glass" element = {<Glass/>} />
+     <Route path = "/Waste/Trash" element = {<Trash/>} />
     
 
      </Routes>
