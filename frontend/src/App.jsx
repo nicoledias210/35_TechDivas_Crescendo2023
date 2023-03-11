8734a18025873ac919039412367d554e4b0db674
Vanessa Dmello <dmello.vanessa2001@gmail.com>
2023-03-12 04:05:54 +0530

    Webcam, upload images

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,29 +1,26 @@
-import React from 'react';
-import './App.css';
+import React from "react";
+import "./App.css";
 
-import Report from './pages/Report';
-import Auth from './pages/Auth'
-import Analytics from './pages/Analytics'
-import {Routes, Route } from 'react-router-dom';
-import Home from './pages/Home';
-import About from './pages/About';
+import Report from "./pages/Report";
+import Auth from "./pages/Auth";
+import Analytics from "./pages/Analytics";
+import { Routes, Route } from "react-router-dom";
 
-  
+import Home from "./pages/Home";
+import About from "./pages/About";
+import WebcamData from "./pages/LiveWebCam";
+
 function App() {
-  return (
-    <>
-    <Routes>
-
-     <Route path = "/Report" element = {<Report/>} />
-     <Route path = "/Analytics" element = {<Analytics/>} />
-     <Route path = "/Auth" element = {<Auth/>} />
-    
-
-     </Routes>
-
-
-    </>
-  );
+	return (
+		<>
+			<Routes>
+				<Route path="/Report" element={<Report />} />
+				<Route path="/Analytics" element={<Analytics />} />
+				<Route path="/Auth" element={<Auth />} />
+				<Route path="/Webcam" element={<WebcamData />} />
+			</Routes>
+		</>
+	);
 }
-  
+
 export default App;
